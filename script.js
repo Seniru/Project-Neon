9d556a647404af68b8c43204e6fcec1bcbff6274
Seniru Pasan Indira <34127015+Seniru@users.noreply.github.com>
2019-02-06 18:52:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //js
+$(document).ready(()=>{
+  $(".full-view").css("height",window.innerHeight+"px");
+  $(".burger").click(()=>{
+    $(".ddlsup").animate({"left":$(".burger").attr("value")=="false"?"0%":"100%"},400);
+    $(".ddl").animate({"left":$(".burger").attr("value")=="false"?"60%":"100%"},400);
+    $(".burger").attr("value",$(".burger").attr("value")=="false"?"true":"false");
+  });
+  $(".ddl > li").click(()=>{
+    $(".ddlsup").animate({"left":"100%"},400);
+    $(".ddl").animate({"left":"100%"},400);
+    $(".burger").attr("value","false");
+  });
+});

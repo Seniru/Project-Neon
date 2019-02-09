646c6cb977c40678a34492235fd198e1758b54c4
Seniru Pasan Indira <34127015+Seniru@users.noreply.github.com>
2019-02-09 14:02:29 +0530

    added some contents. Improved UI
    Removed the effect of the title internet of things as it shows an unexpected behaviour with nav bar on mobile devices

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 //js
 $(document).ready(()=>{
   $(".full-view").css("height",window.innerHeight+"px");
+  /*if($(".full-view").hasChild("half-view")) {
+    $(".full-view").css({"display":"grid"});
+  }*/
   $("body").prepend(`<nav class='main-nav'><ul><li> HOME </li><li> USAGE </li><li> ABOUT </li></ul><button class='burger fas fa-bars' value='false'></button></nav><div class='ddlsup full-view'></div><ul class='ddl full-view' ><i class='fas fa-times-circle' onclick="$('.burger').trigger('click')" style="
     position: relative;
     left: 70%;
     font-size: 20px;
 "></i></ul>`);
-  $('body').append(`<footer class='main-footer full-view'><div class='footer-cell'><img src="images/ioticon.png" style="position:relative;border-radius:50%;width: 40%;height: 40%;/* height: 99px; */top: 9% !important;left: 30% !important;" class="footer-logos"><img class="footer-logos" src="images/ioticon.png" style="position:relative;border-radius:50%;width: 40%;height: 40%;top: 54%!important;left: -10%!important;"></div><section><div class='footer-cell' style='padding: 15px 0px 0px 5px;'><a class='fab fa-facebook-square'><b> Find us on Facebook!</b></a> <br /><a class='fas fa-globe-americas'> Visit our website</a> <br /></div><div class='footer-cell'><input type='text' placeholder='Feedback' class='feed' /><button class='sendmail'>Send</button><hr /> Copyright (c) 2019 MCICTS All Rights Reserved.<div></section></footer>`);
-  $(".burger").click(()=>{
+  $('body').append(`<footer class='main-footer' style="height:`+window.innerHeight+`px">
+      <div class='footer-cell logobar'>
+        <img src="images/ioticon.png" alt="">
+        <img src="images/ioticon.png" alt="">
+      </div>
+      <section class="contact">
+      <div class="footer-cell">
+        <a class="fab fa-facebook-square"> Find us on Facebook!</a><br>
+        <a class="fas fa-globe-americas">Visit our website</a>
+      </div>
+      <div class="footer-cell">
+        <input type="text" id="feed" placeholder="feedback"><button id="submit">Send</button>
+
+      </div>
+      <!--/section-->
+    </footer>`);
+
+
+    $(".burger").click(()=>{
     $(".ddlsup").animate({"left":$(".burger").attr("value")=="false"?"0%":"100%"},400);
     $(".ddl").animate({"left":$(".burger").attr("value")=="false"?"60%":"100%"},400);
     $(".burger").css("color",$(".burger").attr("value")=="false"?"black":"white");
